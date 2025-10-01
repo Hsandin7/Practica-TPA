cf8c3a972d3b6c8d36b18b54111c233c1d097f12
Hsandin7 <123hsandinm@gmail.com>
2025-10-01 13:22:11 +0200

    Tipografía añadida

--- Naipo_Pygame.py
+++ Naipo_Pygame.py
@@ -1,27 +1,49 @@
 import pygame
+import sys
 
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((900, 660))    # Tamaño de la ventana
 pygame.display.set_caption("Naipo")             # Nombre de la ventana
 clock = pygame.time.Clock()                     # Para el manejo del framerate
-running = True
+
+# Variables
+pagina_actual = 0       # 0) Pagina principal   1) 
+
+
+font_test = pygame.font.Font("Testing.ttf", 100)
+
+texto = font_test.render("ABCDEFGH", True, (255,255,255))
+
+def boton():
+    pass
+
+
+class Carta:
+    def __init__(self):
+        pass
+
+
+
+
+
 
 
 # Bucle de juego:
-while running:
+while True:
     for event in pygame.event.get():            # Comprueba si se ha presionado X
         if event.type == pygame.QUIT:
-            running = False
+            pygame.quit()
+            sys.exit()
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill((100,100,100))
 
     # RENDER YOUR GAME HERE
+    screen.blit(texto, (10, 100))       # El .blit permite superponer cualquier cosa por encima de otra
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
 
-pygame.quit()
