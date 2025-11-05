dcdced7c60310a6e9207525b44bfaab757c6c188
Sangafer <sngafer@gmail.com>
2025-11-05 18:57:26 +0100

    Nacho: Organizacion de la clase Comodines

--- src/comodines.py
+++ src/comodines.py
@@ -1,7 +1,6 @@
 import pygame
 import random
 from src.boton import Boton
-# Hay que pensar que atributos tien y que hace cada comodin
 
 efectos_comodin={}
 def registrar_efecto(nombre):
@@ -9,9 +8,6 @@ def registrar_efecto(nombre):
         efectos_comodin[nombre]=func
         return func
     return decorator
-
-#Definicion de efectos para cada comodin
-
 
 @registrar_efecto("gloton")
 def efecto_gloton(mano, fichas, multi, dinero, cartas_jugadas):
@@ -66,9 +62,7 @@ def efecto_esteroides(mano, fichas, multi, dinero, cartas_jugadas):
     
     for _ in cartas_jugadas:
         fichas+=20 
-    # for m in cartas_jugadas:
-    #     fichas = fichas - m.valor
-        
+
     return fichas,multi, dinero
 
 @registrar_efecto("programador")
@@ -83,8 +77,6 @@ def efecto_programador(mano, fichas, multi, dinero, cartas_jugadas):
 
     return fichas,multi, dinero
     
-
-# Ahora mismo esta igual que la clase carta
 class Comodin(Boton):
     _descripciones = {
         "gloton":       "Por cada carta jugada se suman 50 puntos y 10 de multi",
@@ -99,77 +91,63 @@ class Comodin(Boton):
     }
     
     def __init__(self, nombre):
+        # Declaracion de los atributos
         super().__init__(f"Graficos/Comodines/{nombre}.png", 0,0)
         self.nombre = nombre
         self.descripcion = Comodin._descripciones[nombre]
         self.rareza = str() # "Comun", "Raro", "Epico"
         self.precio = int() # Precio de la carta
+        # Declaracion de los estados de interacción
         self.seleccionada = False
-
-        self._registrar_propiedades()
         self.arrastrado=False
-        self.offset_x=0
-        self.offset_y=0
-
-    def aplicar(self, mano, fichas, multi, dinero, cartas_jugadas, comodines):
-        print(self.nombre)
-        print(f"Operacion antes: {fichas} + {multi}")
-        if cartas_jugadas is None:
-            cartas_jugadas = []
+        self.offset_x=self.offset_y=0
         
+        # Configuracion de rareza y precio del comodín en tienda según su rareza
+        self._registrar_propiedades()
+
+    # Metodo aplicar
+    def aplicar(self, mano, fichas, multi, dinero, cartas_jugadas, comodines):
         if self.nombre == "clon":
             for d in reversed(comodines):
                 if d is not self:
                     return d.aplicar(mano,fichas,multi,dinero,cartas_jugadas,comodines)
             return fichas, multi, dinero
+        
         efecto = efectos_comodin.get(self.nombre)
         if efecto is None:
             return fichas, multi, dinero
         
-        resultado = efecto(mano=mano, fichas=+fichas, multi=multi, dinero=dinero, cartas_jugadas=cartas_jugadas)
-        return resultado
+        return efecto(mano,fichas,multi,dinero,cartas_jugadas or [])
     
     def _registrar_propiedades(self):
-        # Asignar rareza
-        if self.nombre in ["matematico", "stonks", "calculadora"]:
-            self.rareza = "comun"
-        elif self.nombre in ["loco", "doblete", "esteroides"]:
-            self.rareza = "raro"
-        elif self.nombre in ["gloton", "programador", "clon"]:
-            self.rareza = "epico"
-        
-        # Asignar precio basado en rareza
-        match self.rareza:
-            case "comun":
-                self.precio=3
-            case "raro":
-                self.precio=5
-            case "epico":
-                self.precio=8
+        # Asignar rareza y precio en base a la misma
+        match self.nombre:
+            case "matematico" | "stonks" | "calculadora":
+                self.rareza, self.precio = "comun", 3
+            case "loco" | "doblete" | "esteroides":
+                self.rareza, self.precio = "raro", 5
+            case "gloton" | "programador" | "clon":
+                self.rareza, self.precio = "epico", 8
             case _:
-                self.precio=1 # Precio por defecto si hay error
+                self.rareza, self.precio = "Desconocido", 1
 
     def detectar_seleccion(self, eventos):
-        if self.detectar_click(eventos):
-            return True
-        return False
+        return self.detectar_click(eventos)
     
     def mover_comodines(self, eventos, lista_comodines, limite_rect=None):
         for e in eventos:
-            if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
-                if self.rect.collidepoint(e.pos):
-                    self.arrastrado = True
-                    mouse_x, mouse_y = e.pos
-                    self.offset_x = self.rect.x - mouse_x
-                    self.offset_y = self.rect.y - mouse_y
+            if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1 and self.rect.collidepoint(e.pos):
+                self.arrastrado = True
+                mouse_x, mouse_y = e.pos
+                self.offset_x = self.rect.x - mouse_x
+                self.offset_y = self.rect.y - mouse_y
 
             elif e.type == pygame.MOUSEBUTTONUP and e.button == 1:
                 if self.arrastrado:
                     self.arrastrado = False
                     for o in lista_comodines:
                         if o is not self and self.rect.colliderect(o.rect):
-                            i = lista_comodines.index(self)
-                            j = lista_comodines.index(o)
+                            i,j = lista_comodines.index(self),lista_comodines.index(o)
                             lista_comodines[i], lista_comodines[j] = lista_comodines[j], lista_comodines[i]
                             break
 
